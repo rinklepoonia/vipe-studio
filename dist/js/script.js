74acf4ec41d98f885a7bb288339bbe40f72a2f74
rinklepoonia <rinklepoonia889@gmail.com>
2024-01-19 15:44:54 +0530

    first commit

---
+++ dist/js/script.js
@@ -0,0 +1,28 @@
+const menuIcon = document.querySelector(".menuIcon")
+const menulist = document.querySelector(".menulist")
+
+menuIcon.addEventListener("click", function () {
+    menulist.classList.toggle("!right-0");
+    menuIcon.classList.toggle("cross")
+})
+
+// back-to-top
+
+const topBtn = document.querySelector(".topBtn");
+const show = document.querySelector(".show");
+
+window.addEventListener("scroll", function () {
+    if (window.scrollY > 60) {
+        topBtn.classList.add("show");
+    } else {
+        topBtn.classList.remove("show");
+    }
+})
+
+topBtn.addEventListener("click", function () {
+    document.documentElement.scrollTop = 0;
+
+})
+function topfuncition() {
+    window.scrollTo({ behavior: "smooth" });
+}
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'Inter': "'Inter' , sans-serif",
+        'Jost': "'Jost' , sans-serif",
+        'gilroy':('')
+      },
+    },
+  },
+  plugins: [],
+}
+
